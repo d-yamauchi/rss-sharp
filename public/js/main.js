8460062c566b163bdb9e8edb9a89760153fe899f
d-yamauchi <d-yamauchi@tes.co.jp>
2019-02-17 15:53:42 +0900

    メインページを作った

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+{
+  const UPDATE_INTERVAL = 5 * 60 * 1000;
+
+  const firstTime = new Date();
+  firstTime.setDate(firstTime.getDate() - 1);
+  const app = new Vue({
+    el: "#application",
+    data: {
+      lastUpdateDate: firstTime,
+      items: [],
+    },
+    methods: {
+      async update() {
+        const response = await fetch(`items?start=${this.getDateString(this.lastUpdateDate)}`);
+        const json = await response.json();
+
+        const items = json.items;
+        for (const item of items) {
+          const site = json.sites.find(site => site.id = item.site_id);
+          item.siteName = site.name;
+          item.hatenaUrl = `http://b.hatena.ne.jp/entry/${item.url}`;
+        }
+        this.items.push(...json.items);
+
+        this.lastUpdateDate = new Date();
+      },
+      getDateString(date) {
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      }
+    },
+  });
+
+  const loop = () => {
+    app.update();
+    setTimeout(loop, UPDATE_INTERVAL);
+  }
+
+  loop();
+}
