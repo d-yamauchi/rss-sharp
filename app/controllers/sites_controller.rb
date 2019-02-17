1efaf5788b6087710a0fc71c21548bae8afdf10c
d-yamauchi <d-yamauchi@tes.co.jp>
2019-02-17 17:33:53 +0900

    過去に無効化したURLを再登録できるようにした

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,11 +1,12 @@
 class SitesController < ApplicationController
   def index
-    sites = Site.where('enabled = 1')
+    sites = Site.where enabled: true
 
     render json: sites
   end
 
   def create
+    # パラメータが無いときにエラーにする
     if params['url'] then
       url = params['url']
     else
@@ -13,8 +14,19 @@ class SitesController < ApplicationController
       return
     end
 
-    name = "hoge"
-    site = Site.new(name: name, url: url, enabled: true)
+    # 新規追加ならinsert
+    # 過去に無効化したサイトならupdate
+    disabledSite = Site.find_by enabled: false, url: url
+    if disabledSite then
+      # update
+      disabledSite.enabled = true
+      site = disabledSite
+    else
+      # insert
+      name = "hoge"
+      site = Site.new(name: name, url: url, enabled: true)
+    end
+
     if site.save then
       render json: {error: false}
     else
